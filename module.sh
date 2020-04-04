6f46300d7e4bb6404245f04148c710e89921292b
Christopher Dunn <cdunn@pacificbiosciences.com>
2020-04-04 09:50:42 -0700

    Use python/3
    MJ set this GNU module up for us.

--- module.sh
+++ module.sh
@@ -1,3 +1,3 @@
 module load smrttools/incremental
 module load jdk/1.8.0_71 meson gcc/8.3.0 nim cram boost zlib htslib samtools gtest pbbam pbcopper falconc
-module load snakemake/5.4.3
+module load snakemake/5.4.3-ve
